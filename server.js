b095391607f9a5e8cfaf72337069a485660f84d0
unknown <bradwayne74@gmail.com>
2018-01-25 19:44:30 -0500

    server

--- server.js
+++ server.js
@@ -4,12 +4,35 @@ var express = require("express");
 var bodyParser = require("body-parser");
 var path = require("path");
 
-// Sets up the Express App
 // =============================================================
+
 var app = express();
 var PORT = process.env.PORT || 3000;
-// Sets up the Express app to handle data parsing
+
 app.use(bodyParser.urlencoded({
     extended: false
 }));
+
 app.use(bodyParser.json());
+
+// =============================================================
+
+var reservations = [
+    {
+    dinerName: 'John',
+    phoneNumber: '555-1234',
+    email: 'john1234@gamil.com',
+    nameId: 'john1234'
+    },
+    {
+    dinerName: 'Bob',
+    phoneNumber: '555-5678',
+    email: 'bob5678@gamil.com',
+    nameId: 'bob5678@gmail.com'
+    }
+];
+
+
+app.listen(PORT, function() {
+    console.log('App listening on PORT ' + PORT);
+});
